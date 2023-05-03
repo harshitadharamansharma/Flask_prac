e985462ee13a7a5413c4388ff61323423dedd995
harshita sharma <harshitasharmad@gmail.com>
2023-05-03 16:16:26 +0530

    second commit

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField  # not only a flask thing; framework agnostic form system
+from wtforms.validators import DataRequired
+from flask import flash
 
 
 # Create a flask instance
 app = Flask(__name__)
+app.config['SECRET_KEY']="MY SECRET KEY THAT NO ONE IS SUPPOSED TO KNOW"
+
+#Create a form class
+class NamerForm(FlaskForm):
+    name = StringField("What's Your Name", validators=[DataRequired()])
+    submit = SubmitField("Submit")
+
+
+# BooleanField
+# DataFields
+# DateTimeField
+# RadioField
+# TextAreaField
+
+
+## Validators
+# DataRequired
+# IPAdress
+# flask-WTF docs
+
 
 
 # Create  a route decorator
@@ -17,6 +41,7 @@ app = Flask(__name__)
 def index():
     nname = "haha"
     stuff = "This is <strong>Bold</strong> text."
+    flash("Welcome to our Website!")
     return render_template("index.html", user_name = nname, Stuff_html = stuff)
 
 # FILTERS
@@ -42,7 +67,9 @@ def user(name):
     First_name = name  #"MYMYName"
     stuff = "This is <strong>Bold</strong> Text"
     LList = ["haaha", "dfdfg", "sdsdf", "fklklkd"]
-    return render_template("user.html", First_name=First_name, stuff = stuff, name_list=LList)
+    pizza_list = ["mashroom", "Sweet corn", "cheeze", 67]
+    return render_template("user.html", First_name=First_name, stuff = stuff, name_list=LList, Pizza_Menu = pizza_list)
+
 
 
 #Create custom error Pages
@@ -61,3 +88,18 @@ def internal_server(e):
 
 # if __name__ == '__main__':
 #     index()
+
+
+@app.route('/name', methods=['GET', 'POST'])
+# you usually post a form and get a web page as a response
+def name():
+    name = None
+    form = NamerForm()    # NameerForm class's obhject created by us
+    # Validate Form
+    if form.validate_on_submit():
+        name = form.name.data
+        form.name.data = '' 
+        flash("Form Submitted Successfully!")
+
+    return render_template("name.html", name = name, form = form)
+
